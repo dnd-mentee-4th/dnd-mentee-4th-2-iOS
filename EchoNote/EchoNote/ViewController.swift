d1647062dcbaceb8af9e744b6b7e0c563e2baf2d
kimhyebeen <kimhbin@naver.com>
2021-01-16 13:04:17 +0900

    Create a project for EchoNote

---
+++ EchoNote/EchoNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EchoNote
+//
+//  Created by 김혜빈 on 2021/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
