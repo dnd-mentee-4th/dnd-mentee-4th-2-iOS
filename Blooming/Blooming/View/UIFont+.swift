147b1252919d56c5a89402001f2d2091c80e3135
kimhyebeen <kimhbin@naver.com>
2021-02-07 00:20:54 +0900

    #19. Font - 폰트 파일 수정 및 UIFont extension 추가

---
+++ Blooming/Blooming/View/UIFont+.swift
@@ -0,0 +1,22 @@
+//
+//  UIFont+.swift
+//  Blooming
+//
+//  Created by 김혜빈 on 2021/02/06.
+//
+
+import UIKit
+
+extension UIFont {
+    static func spoqaBold(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Bold", size: size)!
+    }
+    
+    static func spoqaMedium(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Medium", size: size)!
+    }
+    
+    static func spoqaRegular(_ size: CGFloat) -> UIFont {
+        return UIFont(name: "SpoqaHanSansNeo-Regular", size: size)!
+    }
+}
