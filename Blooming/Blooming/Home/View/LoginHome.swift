f7c7e1394a6c869c39b5f353157aa08524b032bb
kimhyebeen <kimhbin@naver.com>
2021-02-06 20:39:23 +0900

    #16. LoginHome - 로그인하지 않았을 경우의 ContainerView 생성

---
+++ Blooming/Blooming/Home/View/LoginHome.swift
@@ -0,0 +1,28 @@
+//
+//  LoginHome.swift
+//  Blooming
+//
+//  Created by 김혜빈 on 2021/02/06.
+//
+
+import UIKit
+
+class LoginHome: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupView()
+    }
+    
+    func setupView() {
+        
+    }
+
+}
