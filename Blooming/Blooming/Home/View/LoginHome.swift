fb488cbde391349d14e7f93f0192ead4f2664d55
kimhyebeen <kimhbin@naver.com>
2021-02-06 20:50:22 +0900

    #16. LoginHome - 배경 이미지뷰 추가

--- Blooming/Blooming/Home/View/LoginHome.swift
+++ Blooming/Blooming/Home/View/LoginHome.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class LoginHome: UIView {
+    // todo - 이미지 소스 수정 필요
+    let imageView = UIImageView(image: UIImage(named: "ic_quiz"))
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,4 +27,19 @@ class LoginHome: UIView {
         
     }
 
+}
+
+// MARK: +UI
+extension LoginHome {
+    // MARK: ImageView
+    private func setupImageView() {
+        self.addSubview(imageView)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 165).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -242).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 73).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 65).isActive = true
+    }
+    
 }
