9d63db5dbc10eca4178bb97c31f2ddb3eb3f3906
kimhyebeen <kimhbin@naver.com>
2021-02-10 22:01:16 +0900

    #17. FlowerHome - 로그인 이후 ContainerView 생성

---
+++ Blooming/Blooming/Home/View/FlowerHome.swift
@@ -0,0 +1,27 @@
+//
+//  EmptyFlowerHome.swift
+//  Blooming
+//
+//  Created by 김혜빈 on 2021/02/10.
+//
+
+import UIKit
+
+class FlowerHome: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setupView()
+    }
+    
+    func setupView() {
+        
+    }
+}
