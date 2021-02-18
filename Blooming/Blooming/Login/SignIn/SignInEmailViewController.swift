65dd6f9127b56dcf0357646bd7173f2352918eb9
kimhyebeen <kimhbin@naver.com>
2021-02-19 04:04:51 +0900

    #26. Login - SignIn - Email - ViewController 생성

---
+++ Blooming/Blooming/Login/SignIn/SignInEmailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SignInViewController.swift
+//  Blooming
+//
+//  Created by 김혜빈 on 2021/02/19.
+//
+
+import UIKit
+
+class SignInEmailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+    
+    func setupView() {
+        self.view.backgroundColor = UIColor(named: "white02")!
+    }
+
+}
