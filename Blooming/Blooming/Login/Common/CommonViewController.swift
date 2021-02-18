b30b2e4eee90772384661ee0b50ccb6c0aa39bd7
kimhyebeen <kimhbin@naver.com>
2021-02-19 03:40:53 +0900

    #24. Login - Common - 로그인 전 둘러보기 버튼 생성

--- Blooming/Blooming/Login/Common/CommonViewController.swift
+++ Blooming/Blooming/Login/Common/CommonViewController.swift
@@ -15,6 +15,16 @@ class CommonViewController: UIViewController {
             .kern: -0.26
           ])
     }
+    let beforeSignInButton = UIButton().then {
+        $0.setAttributedTitle(NSAttributedString(string: "로그인 전 둘러보기", attributes: [
+            .font: UIFont.spoqaRegular(14),
+            .foregroundColor: UIColor(named: "greyishBrown")!,
+            .kern: -0.14
+        ]), for: .normal)
+    }
+    let underLine = UIView().then {
+        $0.backgroundColor = UIColor(named: "greyishBrown")!
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +35,45 @@ class CommonViewController: UIViewController {
     func setupView() {
         self.view.backgroundColor = UIColor(named: "white02")!
         setupBaseLabel()
+        setupBeforeSignInButton()
+        setupUnderLine()
+    }
+    
+    @objc func clickBeforeSignInButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
 
 }
 
-// +UI
+// MARK: +UI
 extension CommonViewController {
-    // Base Label
+    // MARK: Base Label
     private func setupBaseLabel() {
         self.view.addSubview(baseLabel)
         
         baseLabel.translatesAutoresizingMaskIntoConstraints = false
         baseLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 72).isActive = true
         baseLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 21).isActive = true
+    }
+    
+    // MARK: Before SignIn Button
+    private func setupBeforeSignInButton() {
+        beforeSignInButton.addTarget(self, action: #selector(clickBeforeSignInButton(_:)), for: .touchUpInside)
+        self.view.addSubview(beforeSignInButton)
+        
+        beforeSignInButton.translatesAutoresizingMaskIntoConstraints = false
+        beforeSignInButton.heightAnchor.constraint(equalToConstant: 47).isActive = true
+        beforeSignInButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        beforeSignInButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -32).isActive = true
+    }
+    
+    private func setupUnderLine() {
+        self.view.addSubview(underLine)
+        
+        underLine.translatesAutoresizingMaskIntoConstraints = false
+        underLine.widthAnchor.constraint(equalToConstant: 114).isActive = true
+        underLine.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        underLine.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        underLine.bottomAnchor.constraint(equalTo: beforeSignInButton.bottomAnchor, constant: -12).isActive = true
     }
 }
