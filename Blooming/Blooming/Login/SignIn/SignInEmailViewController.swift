6c60a2c09fb2813a3785d44e11455d1448b67d51
kimhyebeen <kimhbin@naver.com>
2021-02-19 04:23:59 +0900

    #26. Login - SignIn - Email - 이메일 텍스트 필드 생성

--- Blooming/Blooming/Login/SignIn/SignInEmailViewController.swift
+++ Blooming/Blooming/Login/SignIn/SignInEmailViewController.swift
@@ -22,6 +22,19 @@ class SignInEmailViewController: UIViewController {
             .kern: -0.15
           ])
     }
+    let emailField = UITextField().then {
+        $0.attributedPlaceholder = NSAttributedString(string: "이메일", attributes: [
+            .font: UIFont.spoqaMedium(18),
+            .foregroundColor: UIColor(named: "grey05")!,
+            .kern: -0.18
+          ])
+        $0.clearButtonMode = .whileEditing
+        $0.font = .spoqaMedium(18)
+        $0.textColor = UIColor(named: "black")!
+    }
+    let underLine = UIView().then {
+        $0.backgroundColor = UIColor(named: "grey04")!
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +46,8 @@ class SignInEmailViewController: UIViewController {
         self.view.backgroundColor = UIColor(named: "white02")!
         setupWelcomeLabel()
         setupBaseLabel()
+        setupEmailField()
+        setupUnderLine()
     }
 
 }
@@ -55,5 +70,25 @@ extension SignInEmailViewController {
         baseLabel.translatesAutoresizingMaskIntoConstraints = false
         baseLabel.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 8).isActive = true
         baseLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 21).isActive = true
+    }
+    
+    // MARK: Email Field
+    func setupEmailField() {
+        self.view.addSubview(emailField)
+        
+        emailField.translatesAutoresizingMaskIntoConstraints = false
+        emailField.topAnchor.constraint(equalTo: baseLabel.bottomAnchor, constant: 78).isActive = true
+        emailField.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 21).isActive = true
+        emailField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -21).isActive = true
+    }
+    
+    func setupUnderLine() {
+        self.view.addSubview(underLine)
+        
+        underLine.translatesAutoresizingMaskIntoConstraints = false
+        underLine.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        underLine.topAnchor.constraint(equalTo: emailField.bottomAnchor, constant: 8).isActive = true
+        underLine.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 21).isActive = true
+        underLine.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -21).isActive = true
     }
 }
