84840f935f7d4c1961912e092b6385b73e6e66d0
kimhyebeen <kimhbin@naver.com>
2021-02-19 03:09:23 +0900

    #24. Login - Common - ViewContoller 생성

---
+++ Blooming/Blooming/Login/Common/CommonViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CommonViewController.swift
+//  Blooming
+//
+//  Created by 김혜빈 on 2021/02/19.
+//
+
+import UIKit
+
+class CommonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+    
+    func setupView() {
+        
+    }
+
+}
