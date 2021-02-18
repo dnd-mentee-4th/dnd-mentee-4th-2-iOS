26aa9abb40b35d6ca596efa38395ad917a2b008c
kimhyebeen <kimhbin@naver.com>
2021-02-19 03:11:59 +0900

    #24. Login - Common - Base Label 생성

--- Blooming/Blooming/Login/Common/CommonViewController.swift
+++ Blooming/Blooming/Login/Common/CommonViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class CommonViewController: UIViewController {
+    let baseLabel = UILabel().then {
+        $0.attributedText = NSAttributedString(string: "반가워요 :)", attributes: [
+            .font: UIFont.spoqaBold(26),
+            .foregroundColor: UIColor(named: "black")!,
+            .kern: -0.26
+          ])
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,4 +26,16 @@ class CommonViewController: UIViewController {
         
     }
 
+}
+
+// +UI
+extension CommonViewController {
+    // Base Label
+    private func setupBaseLabel() {
+        self.view.addSubview(baseLabel)
+        
+        baseLabel.translatesAutoresizingMaskIntoConstraints = false
+        baseLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 72).isActive = true
+        baseLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 21).isActive = true
+    }
 }
