232efad64aff6cfe16fedce4dc270b58e0e387a9
kimhyebeen <kimhbin@naver.com>
2021-02-19 12:23:14 +0900

    #29. Login - Password - 비밀번호 텍스트 필드 생성

--- Blooming/Blooming/Login/SignIn/SignInPasswordViewController.swift
+++ Blooming/Blooming/Login/SignIn/SignInPasswordViewController.swift
@@ -14,11 +14,17 @@ class SignInPasswordViewController: UIViewController {
     let backButton = UIButton()
     let welcomeLabel = UILabel()
     let baseLabel = UILabel()
+    let passwordField = UITextField()
+    let underLine = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupView()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     func setupView() {
@@ -27,6 +33,8 @@ class SignInPasswordViewController: UIViewController {
         setupBackButton()
         setupWelcomeLabel()
         setupBaseLabel()
+        setupEmailField()
+        setupUnderLine()
     }
     
     @objc func clickBackButton(_ sender: UIButton) {
@@ -76,5 +84,35 @@ extension SignInPasswordViewController {
         baseLabel.translatesAutoresizingMaskIntoConstraints = false
         baseLabel.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 8).isActive = true
         baseLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 21).isActive = true
+    }
+    
+    // MARK: Password Field
+    func setupEmailField() {
+        passwordField.attributedPlaceholder = NSAttributedString(string: "비밀번호", attributes: [
+            .font: UIFont.spoqaMedium(18),
+            .foregroundColor: UIColor(named: "grey05")!,
+            .kern: -0.18
+          ])
+        passwordField.clearButtonMode = .whileEditing
+        passwordField.font = .spoqaMedium(18)
+        passwordField.textColor = UIColor(named: "black")!
+        passwordField.isSecureTextEntry = true
+        self.view.addSubview(passwordField)
+        
+        passwordField.translatesAutoresizingMaskIntoConstraints = false
+        passwordField.topAnchor.constraint(equalTo: baseLabel.bottomAnchor, constant: 78).isActive = true
+        passwordField.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 21).isActive = true
+        passwordField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -21).isActive = true
+    }
+    
+    func setupUnderLine() {
+        underLine.backgroundColor = UIColor(named: "grey04")!
+        self.view.addSubview(underLine)
+        
+        underLine.translatesAutoresizingMaskIntoConstraints = false
+        underLine.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        underLine.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 8).isActive = true
+        underLine.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 21).isActive = true
+        underLine.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -21).isActive = true
     }
 }
