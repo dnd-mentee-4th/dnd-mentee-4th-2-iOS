d15027f199063cd2a455c65980e6fcf2e97a899a
kimhyebeen <kimhbin@naver.com>
2021-02-19 12:13:15 +0900

    #29. Login - Password - Back Button 생성

--- Blooming/Blooming/Login/SignIn/SignInPasswordViewController.swift
+++ Blooming/Blooming/Login/SignIn/SignInPasswordViewController.swift
@@ -11,6 +11,7 @@ import RxSwift
 class SignInPasswordViewController: UIViewController {
     private let disposeBag = DisposeBag()
     var viewModel: SignInViewModel?
+    let backButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,27 @@ class SignInPasswordViewController: UIViewController {
     func setupView() {
         self.view.backgroundColor = UIColor(named: "white02")!
         
+        setupBackButton()
+    }
+    
+    @objc func clickBackButton(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
     }
 
+}
+
+// MARK: +UI
+extension SignInPasswordViewController {
+    // MARK: Back Button
+    func setupBackButton() {
+        backButton.setImage(UIImage(named: "ic_btn_back")!, for: .normal)
+        backButton.addTarget(self, action: #selector(clickBackButton(_:)), for: .touchUpInside)
+        self.view.addSubview(backButton)
+        
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.widthAnchor.constraint(equalToConstant: 45).isActive = true
+        backButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
+        backButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
+    }
 }
