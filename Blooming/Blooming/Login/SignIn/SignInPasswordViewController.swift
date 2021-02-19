daa48f625e77a9ae1ef17c88d9a2a4942eedcdc8
kimhyebeen <kimhbin@naver.com>
2021-02-19 12:15:55 +0900

    #29. Login - Password - 베이스 라벨 생성

--- Blooming/Blooming/Login/SignIn/SignInPasswordViewController.swift
+++ Blooming/Blooming/Login/SignIn/SignInPasswordViewController.swift
@@ -12,6 +12,8 @@ class SignInPasswordViewController: UIViewController {
     private let disposeBag = DisposeBag()
     var viewModel: SignInViewModel?
     let backButton = UIButton()
+    let welcomeLabel = UILabel()
+    let baseLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +25,8 @@ class SignInPasswordViewController: UIViewController {
         self.view.backgroundColor = UIColor(named: "white02")!
         
         setupBackButton()
+        setupWelcomeLabel()
+        setupBaseLabel()
     }
     
     @objc func clickBackButton(_ sender: UIButton) {
@@ -44,5 +48,33 @@ extension SignInPasswordViewController {
         backButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
         backButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
         backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10).isActive = true
+    }
+    
+    // MARK: Welcome Label
+    func setupWelcomeLabel() {
+        welcomeLabel.attributedText = NSAttributedString(string: "반가워요 :)", attributes: [
+            .font: UIFont.spoqaBold(26),
+            .foregroundColor: UIColor(named: "black")!,
+            .kern: -0.26
+          ])
+        self.view.addSubview(welcomeLabel)
+        
+        welcomeLabel.translatesAutoresizingMaskIntoConstraints = false
+        welcomeLabel.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 72).isActive = true
+        welcomeLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 21).isActive = true
+    }
+    
+    // MARK: Base Label
+    func setupBaseLabel() {
+        baseLabel.attributedText = NSAttributedString(string: "꽃을 피우러 가볼까요?", attributes: [
+            .font: UIFont.spoqaRegular(15),
+            .foregroundColor: UIColor(named: "grey03")!,
+            .kern: -0.15
+          ])
+        self.view.addSubview(baseLabel)
+        
+        baseLabel.translatesAutoresizingMaskIntoConstraints = false
+        baseLabel.topAnchor.constraint(equalTo: welcomeLabel.bottomAnchor, constant: 8).isActive = true
+        baseLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 21).isActive = true
     }
 }
