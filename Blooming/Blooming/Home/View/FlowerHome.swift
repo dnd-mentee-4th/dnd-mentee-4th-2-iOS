e0ba1dcb4fd3b29ba21763efb4819f7d0baf96a8
kimhyebeen <kimhbin@naver.com>
2021-02-22 00:35:58 +0900

    #17. FlowerHome - 남은 카운팅 라벨 생성

--- Blooming/Blooming/Home/View/FlowerHome.swift
+++ Blooming/Blooming/Home/View/FlowerHome.swift
@@ -13,7 +13,10 @@ struct Flower {
     var color: String
     
     var percent: Int {
-        return Int(score/4 * 100)
+        return Int(score/5 * 100)
+    }
+    var remaining: Int {
+        return 5 - score
     }
 }
 
@@ -23,6 +26,7 @@ protocol FlowerHomeDelegate: class {
 
 class FlowerHome: UIView {
     let percentageLabel = UILabel()
+    let remainingLabel = UILabel()
     let activityButton = UIButton()
     
     weak var delegate: FlowerHomeDelegate?
@@ -41,15 +45,18 @@ class FlowerHome: UIView {
     
     func setupView() {
         setupPercentageLabel()
+        setupRemainingLabel()
         setupActivityButton()
     }
     
     func setFlowerInfo(flower: Flower) {
         if flower.score == 0 {
             percentageLabel.attributedText = getEmptyPercentageText(color: UIColor(named: flower.color)!)
+            remainingLabel.attributedText = getEmptyFlowerRemainingLabel()
             return
         }
         percentageLabel.attributedText = getFlowerPercentageText(color: UIColor(named: flower.color)!, title: flower.title, percent: flower.percent)
+        remainingLabel.attributedText = getNotEmptyFlowerRemainingLabel(count: flower.remaining, color: UIColor(named: flower.color)!)
     }
     
     @objc func clickActivityButton(_ sender: UIButton) {
@@ -92,6 +99,41 @@ extension FlowerHome {
             .foregroundColor: color
         ], range: NSRange(location: 7, length: String(percent).count + 1))
         
+        return attributedString
+    }
+    
+    // MARK: Remaining Label
+    func setupRemainingLabel() {
+        remainingLabel.attributedText = getEmptyFlowerRemainingLabel()
+        self.addSubview(remainingLabel)
+        
+        remainingLabel.translatesAutoresizingMaskIntoConstraints = false
+        remainingLabel.topAnchor.constraint(equalTo: percentageLabel.bottomAnchor, constant: 16).isActive = true
+        remainingLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 32).isActive = true
+    }
+    
+    func getEmptyFlowerRemainingLabel() -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: "꽃을 새롭게 키워보세요", attributes: [
+            .font: UIFont.systemFont(ofSize: 16),
+          .foregroundColor: UIColor(named: "grey03")!,
+          .kern: -0.16
+        ])
+        attributedString.addAttribute(.font, value: UIFont.boldSystemFont(ofSize: 16), range: NSRange(location: 3, length: 3))
+
+        return attributedString
+    }
+
+    func getNotEmptyFlowerRemainingLabel(count: Int, color: UIColor) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: "꽃 수확을 위해 \(count)번 더 실천하세요", attributes: [
+            .font: UIFont.boldSystemFont(ofSize: 16),
+          .foregroundColor: UIColor(named: "grey03")!,
+          .kern: -0.16
+        ])
+        attributedString.addAttributes([
+            .font: UIFont.boldSystemFont(ofSize: 16),
+          .foregroundColor: color
+        ], range: NSRange(location: 9, length: 1))
+
         return attributedString
     }
     
