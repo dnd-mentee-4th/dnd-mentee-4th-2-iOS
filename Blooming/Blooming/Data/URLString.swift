5a5e9e645e271c91a72ca5e29f1d0f41d7455c3a
kimhyebeen <kimhbin@naver.com>
2021-02-22 11:10:16 +0900

    #33. 사용자 API - 요청 메서드 구현

--- Blooming/Blooming/Data/URLString.swift
+++ Blooming/Blooming/Data/URLString.swift
@@ -11,5 +11,5 @@ struct URLString {
     static private let baseUrl: String = "http://ec2-13-124-120-203.ap-northeast-2.compute.amazonaws.com"
     static let login: String = "\(baseUrl)/api/signin"
     static let signup: String = "\(baseUrl)/api/signup"
-    static let user: String = "\(baseUrl)/api/user/"
+    static let user: String = "\(baseUrl)/api/user"
 }
